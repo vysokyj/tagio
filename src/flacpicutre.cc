687e1512616f9e538dde9d045e57848d843b226b
Jiří Vysoký <jiri.vysoky@gmail.com>
2016-05-24 12:34:00 +0200

    Fixed return type

--- src/flacpicutre.cc
+++ src/flacpicutre.cc
@@ -5,7 +5,9 @@ using namespace v8;
 using namespace std;
 
 Local<Object> FLACPictures::New(Isolate *isolate, TagLib::List<TagLib::FLAC::Picture *> list) {
-
+    //TODO: Implement...
+    Local<Object> object = Object::New(isolate);
+    return object;
 }
 
 void FLACPictures::Set(Isolate *isolate, Object *object, TagLib::List<TagLib::FLAC::Picture *> list) {
